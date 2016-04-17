117b637ba2692e6fe1a89829679a614b138671ef
ZerOx4C <zerox4c@gmail.com>
2016-04-17 15:56:08 +0900

    fix canvas size

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var g = null;
 var FPS = 10;
+var RES_X = 640;
+var RES_Y = 480;
 
 var update = function () {
 
 };
 
 window.onload = function () {
-    g = document.getElementById('target').getContext('2d');
+    var canvas = document.getElementById('target');
+    canvas.width = RES_X;
+    canvas.height = RES_Y;
+    g = canvas.getContext('2d');
     setInterval(update, 1000 / FPS);
 };
