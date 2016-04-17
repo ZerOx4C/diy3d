3e9a329383d897bf25057ead624dfea4f79ed3d8
ZerOx4C <zerox4c@gmail.com>
2016-04-17 15:57:33 +0900

    impl draw.segment

--- draw.js
+++ draw.js
@@ -3,4 +3,32 @@ var draw = {
         context.fillStyle = color;
         context.fillRect(x, y, 1, 1);
     },
+    _segmentByX: function (context, xStart, xEnd, yStart, yEnd, color) {
+        var y = yStart;
+        var s = (yEnd - yStart) / (xEnd - xStart);
+        for (var x = xStart; x <= xEnd; ++x) {
+            draw.pixel(context, x, Math.round(y), color);
+            y += s;
+        }
+    },
+    _segmentByY: function (context, xStart, xEnd, yStart, yEnd, color) {
+        var x = xStart;
+        var s = (xEnd - xStart) / (yEnd - yStart);
+        for (var y = yStart; y <= yEnd; ++y) {
+            draw.pixel(context, Math.round(x), y, color);
+            x += s;
+        }
+    },
+    segment: function (context, x1, y1, x2, y2, color) {
+        var dx = x1 - x2;
+        var dy = y1 - y2;
+        if (Math.abs(dy) < Math.abs(dx)) {
+            if (x1 < x2) draw._segmentByX(context, x1, x2, y1, y2, color);
+            else         draw._segmentByX(context, x2, x1, y2, y1, color);
+        }
+        else {
+            if (y1 < y2) draw._segmentByY(context, x1, x2, y1, y2, color);
+            else         draw._segmentByY(context, x2, x1, y2, y1, color);
+        }
+    }
 };
