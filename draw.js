606a4a35bff7a3e95f98548c391637c292ad0890
ZerOx4C <zerox4c@gmail.com>
2016-04-18 03:34:51 +0900

    impl draw.triangle

--- draw.js
+++ draw.js
@@ -30,5 +30,33 @@ var draw = {
             if (y1 < y2) draw._segmentByY(context, x1, x2, y1, y2, color);
             else         draw._segmentByY(context, x2, x1, y2, y1, color);
         }
-    }
+    },
+    _getIntersectionX: function (x1, y1, x2, y2, y) {
+        // segment is horizon.
+        if (y1 == y2) {
+            return null;
+        }
+        // out of range.
+        if (y < Math.min(y1, y2) || Math.max(y1, y2) < y) {
+            return null;
+        }
+        return Math.round((y - y1) * (x1 - x2) / (y1 - y2) + x1);
+    },
+    triangle: function (context, x1, y1, x2, y2, x3, y3, color) {
+        var ix1 = null;
+        var ix2 = null;
+        var yStart = Math.min(y1, y2, y3);
+        var yEnd   = Math.max(y1, y2, y3);
+        for (var y = yStart; y <= yEnd; ++y) {
+            ix1 = draw._getIntersectionX(x1, y1, x2, y2, y);
+            ix2 = draw._getIntersectionX(x2, y2, x3, y3, y);
+            if (ix1 == null) {
+                ix1 = draw._getIntersectionX(x3, y3, x1, y1, y);
+            }
+            else if (ix2 == null) {
+                ix2 = draw._getIntersectionX(x3, y3, x1, y1, y);
+            }
+            draw.segment(context, ix1, y, ix2, y, color);
+        }
+    },
 };
